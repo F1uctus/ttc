d39824d93dcf2824d3562841e5d787bb502ba4f3
Ilya Nikitin <f1uctus@mail.ru>
2022-01-16 01:04:09 +0300

    `russian/pipelines`: Adapt `noun_chunks` from Greek

--- ttc/language/russian/pipelines.py
+++ ttc/language/russian/pipelines.py
@@ -1,9 +1,9 @@
-from typing import Iterator
+from typing import Iterator, Callable, Iterable
 
 from spacy import Errors
 from spacy.language import Language
-from spacy.symbols import NOUN, PROPN, PRON  # type: ignore
-from spacy.tokens import Doc, Span
+from spacy.symbols import NOUN, PROPN, PRON, nmod, amod, NUM, acl  # type: ignore
+from spacy.tokens import Doc, Span, Token
 
 from ttc.language.russian.constants import MISPREDICTED_VERBS
 
@@ -17,49 +17,66 @@ def _correct_mispredictions(doc):
 
 
 # TODO: Adapt to the Russian lang (Copied from Greek pipeline)
+
+def traverse_children(
+    word: Token,
+    children_selector: Callable[[Token], Iterable[Token]],
+    dep_predicate: Callable[[Token], bool],
+):
+    children = list(children_selector(word))
+    max_i = max((i for i, c in enumerate(children) if dep_predicate(c)), default=-1)
+    return (
+        traverse_children(
+            children[max_i],
+            children_selector,
+            dep_predicate,
+        )
+        if max_i > -1
+        else word
+    )
+
+
 def _noun_chunks(doclike: Doc | Span) -> Iterator[tuple[int, int, int]]:
     """Detect base noun phrases from a dependency parse. Works on Doc and Span."""
-    # It follows the logic of the noun chunks finder of English language,
-    # adjusted to some Greek language special characteristics.
-    # obj tag corrects some DEP tagger mistakes.
-    # Further improvement of the models will eliminate the need for this tag.
-    labels = ["nsubj", "obj", "iobj", "appos", "ROOT", "obl"]
     doc = doclike.doc  # Ensure works on both Doc and Span.
+
     if not doc.has_annotation("DEP"):
         raise ValueError(Errors.E029)
-    np_deps = [doc.vocab.strings.add(label) for label in labels]
-    conj = doc.vocab.strings.add("conj")
-    nmod = doc.vocab.strings.add("nmod")
+
     np_label = doc.vocab.strings.add("NP")
+    labels = ["nsubj", "nsubj:pass", "obj", "iobj", "appos", "ROOT", "obl"]
+    np_deps = [doc.vocab.strings.add(label) for label in labels]
+
     prev_end = -1
+
     for i, word in enumerate(doclike):
-        if word.pos not in (NOUN, PROPN, PRON):
-            continue
         # Prevent nested chunks from being produced
         if word.left_edge.i <= prev_end:
             continue
-        if word.dep in np_deps:
-            flag = False
-            if word.pos == NOUN:
-                #  check for patterns such as γραμμή παραγωγής
-                for potential_nmod in word.rights:
-                    if potential_nmod.dep == nmod:
-                        prev_end = potential_nmod.i
-                        yield word.left_edge.i, potential_nmod.i + 1, np_label
-                        flag = True
-                        break
-            if flag is False:
-                prev_end = word.i
-                yield word.left_edge.i, word.i + 1, np_label
-        elif word.dep == conj:
-            # covers the case: έχει όμορφα και έξυπνα παιδιά
-            head = word.head
-            while head.dep == conj and head.head.i < head.i:
-                head = head.head
-            # If the head is an NP, and we're coordinated to it, we're an NP
-            if head.dep in np_deps:
-                prev_end = word.i
-                yield word.left_edge.i, word.i + 1, np_label
+
+        if word.pos not in (NOUN, PROPN, PRON, NUM):
+            continue
+
+        if word.dep not in np_deps:
+            continue
+
+        start = traverse_children(
+            word,
+            lambda t: t.lefts,
+            lambda t: t.dep_ in ("nmod", "amod", "obl", "appos"),
+        )
+
+        end = traverse_children(
+            word,
+            lambda t: t.rights,
+            lambda t: (
+                t.dep_ in ("nmod", "amod", "acl", "obl", "case", "appos")
+                or t.dep_.startswith("acl")
+            ),
+        )
+
+        prev_end = end.i
+        yield start.i, end.i + 1, np_label
 
 
 def setup_language(lang: Language):
