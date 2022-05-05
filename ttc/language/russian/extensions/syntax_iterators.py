32404e662ae3f928f54fa6faeff229899452e530
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-06 00:31:21 +0300

    `russian/.../syntax_iterators.py`: Rewrite the `noun_chunks` iterator
    Improve accuracy of the `noun_chunks` iterator.
    Implementation mostly borrowed from corresponding spaCy iterator for the Danish language.
    Add `noun_chunks_comparison` notebook with corresponding tests.

--- ttc/language/russian/extensions/syntax_iterators.py
+++ ttc/language/russian/extensions/syntax_iterators.py
@@ -1,66 +1,121 @@
-from typing import Iterator, Callable, Iterable, Union, Tuple
+from typing import Iterator, Union, Tuple, Optional, Collection
 
 from spacy import Errors
-from spacy.symbols import NOUN, PROPN, PRON, nmod, amod, NUM, acl, PART  # type: ignore
+from spacy.symbols import NOUN, PROPN, PRON, NUM, VERB, AUX, ADP, punct  # type: ignore
 from spacy.tokens import Doc, Span, Token
 
 
-def _traverse_children(
-    word: Token,
-    children_selector: Callable[[Token], Iterable[Token]],
-    dep_predicate: Callable[[Token], bool],
-):
-    children = list(children_selector(word))
-    max_i = max((i for i, c in enumerate(children) if dep_predicate(c)), default=-1)
-    return (
-        _traverse_children(
-            children[max_i],
-            children_selector,
-            dep_predicate,
-        )
-        if max_i > -1
-        else word
-    )
+def next_with_pos(
+    *,
+    from_: Token,
+    max_distance: int = 2**32,
+    poss: Collection[int],
+) -> Optional[Token]:
+    max_distance = min(max_distance, len(from_.doc) - from_.i)
+    if from_.dep == punct:
+        try:
+            from_ = from_.nbor()
+        except IndexError:
+            return None
+    i = 1
+    while i < max_distance:
+        from_ = from_.nbor()
+        if from_.pos in poss:
+            return from_
+        i += 1
+    return None
 
 
 def noun_chunks(doclike: Union[Doc, Span]) -> Iterator[Tuple[int, int, int]]:
-    """Detect base noun phrases from a dependency parse. Works on Doc and Span."""
+    def is_verb_token(tok):
+        return tok.pos in [VERB, AUX]
+
+    def get_left_bound(root):
+        left_bound = root
+        for tok in reversed(list(root.lefts)):
+            if tok.dep in np_left_deps:
+                left_bound = tok
+        return left_bound
+
+    def get_right_bound(root):
+        right_bound = root
+        for tok in root.rights:
+            if tok.dep in np_right_deps:
+                right = get_right_bound(tok)
+                if list(filter(lambda t: is_verb_token(t), doc[root.i : right.i])):
+                    break
+                else:
+                    right_bound = right
+        complement_np_adp = next_with_pos(from_=right_bound, max_distance=2, poss={ADP})
+        if complement_np_adp:
+            complement_np_noun = next_with_pos(from_=complement_np_adp, poss=nn_poss)
+            if complement_np_noun and complement_np_noun.is_ancestor(complement_np_adp):
+                right_bound = get_right_bound(complement_np_noun)
+        return right_bound
+
+    def get_bounds(root):
+        return get_left_bound(root), get_right_bound(root)
+
     doc = doclike.doc  # Ensure works on both Doc and Span.
 
     if not doc.has_annotation("DEP"):
         raise ValueError(Errors.E029)
 
+    if not len(doc):
+        return
+
+    left_labels = [
+        "det",
+        "case",
+        "fixed",
+        "nmod:poss",
+        "nmod",
+        "amod",
+        "flat",
+        "goeswith",
+        "nummod",
+        "obl",
+        "appos",
+    ]
+    right_labels = [
+        "fixed",
+        "nmod:poss",
+        "nmod",
+        "amod",
+        "flat",
+        "goeswith",
+        "nummod",
+        "obl",
+        "appos",
+    ]
+
     np_label = doc.vocab.strings.add("NP")
-    labels = ["nsubj", "nsubj:pass", "obj", "iobj", "appos", "ROOT", "obl"]
-    np_deps = [doc.vocab.strings.add(label) for label in labels]
+    np_left_deps = {doc.vocab.strings.add(label) for label in left_labels}
+    np_right_deps = {doc.vocab.strings.add(label) for label in right_labels}
+    nn_poss = {PROPN, NOUN, PRON, NUM}
 
-    prev_end = -1
+    prev_right = -1
+    for token in doclike:
+        if token.pos in nn_poss:
+            left, right = get_bounds(token)
+            if left.i <= prev_right:
+                continue
+            if token.pos == NUM and left.i == right.i:
+                continue
+            yield left.i, right.i + 1, np_label
+            prev_right = right.i
 
-    for i, word in enumerate(doclike):
-        # Prevent nested chunks from being produced
-        if word.left_edge.i <= prev_end:
-            continue
 
-        if word.pos not in (NOUN, PROPN, PRON, NUM):
-            continue
+if __name__ == "__main__":
+    import spacy
+    import ttc.language.russian.pipelines as russian_pipelines
 
-        if word.dep not in np_deps:
-            continue
+    nlp = spacy.load("ru_core_news_sm")
+    russian_pipelines.register_for(nlp)
 
-        start = _traverse_children(
-            word,
-            lambda t: t.lefts,
-            lambda t: t.dep_ in ("nmod", "amod", "obl", "appos"),
-        )
+    text = "Один из них, худощавый, с подвижным лицом, приблизился к Гусеву и быстро проговорил."
 
-        end = _traverse_children(
-            word,
-            lambda t: t.rights,
-            lambda t: (
-                t.dep_ in ("nmod", "amod", "acl", "obl", "case", "appos")
-                or t.dep_.startswith("acl")
-            ),
-        )
-
-        prev_end = end.i
-        yield start.i, end.i + 1, np_label
+    nlp.vocab.get_noun_chunks = noun_chunks
+    doc = nlp(text)
+    ncs = [str(nc) for nc in doc.noun_chunks]
+    print(ncs)
